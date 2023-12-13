58666996bb61e18e3fc4c3bbd8c76b63cbb5277b
partkyle <partkyle@gmail.com>
2023-12-13 11:44:55 -0800

    it's better. I understand it now. I made some dumb choices and tried to rush too much.
    I think spending another minute instead of copy paste logging would have saved quit a bit of time.

--- src/day13/index.ts
+++ src/day13/index.ts
@@ -1,7 +1,15 @@
 import run from "aocrunner"
 import { log } from "console"
-import { LOADIPHLPAPI } from "dns"
-import _, { toLower } from "lodash"
+import _, { sum, toLower } from "lodash"
+
+let DEBUG = false
+function debug(message?: any, ...optionalParams: any[]) {
+  if (DEBUG) {
+    if (message) {
+      log(message, ...optionalParams)
+    }
+  }
+}
 
 const parseInput = (rawInput: string) => rawInput.split('\n\n').map( section => section.split('\n'))
 
@@ -11,30 +19,16 @@ const part1 = (rawInput: string) => {
   let results = input.map( s => getReflectValue(s))
 
   return _.sum(results)
-
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  let tests = [
-    ['asdf', 'asdf'],
-    ['asdf', 'fsdf'],
-    ['asdf', 'ffdf'],
-    ['......', '......'],
-    ['......', '.....#'],
-    ['##......#', '##......#'],
-  ]
-
-  for (let [a,b] of tests) {
-    log(diffByOnly1(a,b), a, b)
-  }
-
+  DEBUG = true
 
   let results = input.map( s => getReflectValue(s, true))
 
   return _.sum(results)
-
 }
 
 run({
@@ -92,104 +86,41 @@ run({
   onlyTests: false,
 })
 
-
 function getReflectValue(s: string[], tolerant: boolean = false) {
-  let h = horizontalReflect(s)
-  let v = verticalReflect(s)
+  let h = horizontalReflect(s, tolerant)
+  let v = verticalReflect(s, tolerant)
 
   return h * 100 + v
 }
 
+
 function horizontalReflect(s: string[], tolerant: boolean = false) {
-  let maxcount = 0;
-  let maxindex = 0
-  for (let i = 0; i < s.length; i++) {
-    // check reflection count
-    let count = 0
+  let sumCompare = tolerant ? 1 : 0
 
-    let diffByOnlyOneOnCooldown = false
 
-    let prev = s[i - 1]
-    let curr = s[i]
-    if (_.isEqual(prev, curr) || diffByOnly1(prev, curr)) {
-      if (diffByOnly1(prev, curr)) {
-        diffByOnlyOneOnCooldown = true
+  let indexes = []
+  for (let i = 1; i < s.length; i++) {
+    let rowsum = 0
+    for (let r = 0; r < Math.min(i, s.length - i); r++) {
+      let [left, right] = [s[i-r-1], s[i+r]]
+
+      let sum = 0
+      for (let [lch, rch] of _.zip(left.split(''), right.split(''))) {
+        sum += lch == rch ? 0 : 1
       }
-      count++
 
-      for (let r = 1; r < i; r++) {
-        let up = s[i - r - 1]
-        let down = s[i + r]
-
-        if (up === undefined || down === undefined || _.isEqual(up, down) || (!diffByOnlyOneOnCooldown && diffByOnly1(up, down))) {
-          if (diffByOnly1(up, down)) {
-            diffByOnlyOneOnCooldown = true
-          }
-          count++
-        } else {
-          count = 0
-          break
-        }
-      }
+      rowsum += sum
     }
 
-    if (!diffByOnlyOneOnCooldown) {
-      continue
+    if (rowsum == sumCompare) {
+      indexes.push(i)
     }
+  }
 
-    if (maxcount < count) {
-      maxcount = count
-      maxindex = i
-    }
-
-   }
-
-   return maxindex 
+  return indexes.length == 0 ? 0 : Math.max(...indexes)
 }
 
 function verticalReflect(s: string[], tolerant: boolean = false) {
   let newmap = _.range(0, s[0].length).map( i => s.map( line => line[i] ).reverse().join(''))
-
-  return horizontalReflect(newmap)
-
-  let maxcount = 0;
-  let maxindex = 0
-  for (let i = 0; i < s[0].length; i++) {
-    // check reflection count
-    let count = 0
-
-
-    if (_.isEqual(s.map( l => l[i-1] ), s.map( l => l[i] ))) {
-      count++
-
-      for (let r = 1; r <= i; r++) {
-        let index = i
-        log(s.map( l => l[index + r] ).join(''), s.map( l => l[index - r] ).join(''))
-        if (_.isEqual(s.map( l => l[index + r] ), s.map( l => l[index - r] ))) {
-          count++
-        }
-      }
-    }
-    
-    if ( count < maxindex ) {
-      maxcount = count
-      maxindex = i
-    }
-
-    let prev = s.map( l => l[i-1] ).join('')
-    let current = s.map( l => l[i] ).join('')
-
-    log(i, prev, current, _.isEqual(prev, current), maxcount, maxindex)
-  }
-  return maxindex 
-}
-
-
-function diffByOnly1(a: string, b: string) {
-  if (a === undefined || b === undefined) {
-    return false
-  }
-
-  let result = _.zip(a.split(''), b.split('')).map( ([l,r]) => l != r).filter(e => e).length == 1
-  return result
+  return horizontalReflect(newmap, tolerant)
 }
